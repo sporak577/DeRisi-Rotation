19062cd58cea2e6d1cc3a885d35604b03cd53349
sporak577 <sophie.porak@ucsf.edu>
2025-05-09 14:36:29 -0700

    visual representation of tile distribution representing family, genus, strains

---
+++ phipseq/data analysis/virus_in_tiles_representation.py
@@ -0,0 +1,40 @@
+"""
+Input: 
+a CSV file of the metadata of the phage-display tiles that we sent off for ordering. 
+
+this file has following columns: 
+accession, count, protein_name, organism, isolate, host, geo_loc_name, 
+collection_date, segment, strain, gb_accession, taxnonmy_lineage
+
+Output: 
+Pie chart for family, genus and species distribution. 
+"""
+
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('/Users/sophieporak/Library/CloudStorage/Box-Box/DeRisi/data processing/lib_data_AK_050725/0.96_tiling_out_nt_tiles_cp_cleaned_metadata_curated.csv')
+
+date = "050925"
+
+# Function to make pie chart with 2% cutoff
+def plot_pie(series, label, outname):
+    counts = series.value_counts() #index is the category and value is the count
+    total = counts.sum() #total number of items (sum of all counts)
+    filtered = counts[counts / total >= 0.02] #filters counts to only keep labels that represent omore or equal 2% of total 
+    other_sum = total - filtered.sum()
+    
+    if other_sum > 0: #sums how many items were excluded
+        filtered["Other (<2%)"] = other_sum
+
+    plt.figure(figsize=(8, 8))
+    plt.pie(filtered, labels=filtered.index, autopct='%1.1f%%', startangle=140)
+    plt.title(f"Tile Count Representation of Viral {label}")
+    plt.tight_layout()
+    plt.savefig(f"/Users/sophieporak/Desktop/{outname}_{date}.png")
+    plt.close()
+
+# Apply to different taxonomic levels
+plot_pie(df['tax_rank_7'], "Families", "virus_family_distribution_pie")
+plot_pie(df['tax_rank_8'], "Genera", "virus_genus_distribution_pie")
+plot_pie(df['tax_rank_9'], "Strains", "virus_strain_distribution_pie")
