73ed6c5e193127795ab1e116e146f80122e4670e
sporak577 <sophie.porak@ucsf.edu>
2025-07-21 08:52:06 -0700

    Update add_linkers.py

--- add_linkers.py
+++ add_linkers.py
@@ -11,8 +11,8 @@ flank_5 = "GTGGTTGGTGCTGTAGGAGCA"
 flank_3 = "TGATAAGCATATGCCATGGCCTC"
 
 # Input and output files
-input_fasta = "/Users/sophieporak/Desktop/0.94_final_library_protein_records_tiling_out/0.94_tiling_out_nt_tiles.fasta"
-output_fasta = "0.94_tiling_out_nt_tiles_with_flanks.fasta"
+input_fasta = "your_input_file.fasta"
+output_fasta = "your_output_file.fasta"
 
 # Read input FASTA and add flanks
 updated_records = []
